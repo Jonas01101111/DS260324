9325609979c0bc209a1c2c839e961933aeb0c4ac
Usuario <Usuario@CPD02-M08>
2024-03-26 10:24:26 -0300

    :D

--- NumerosInteiros/Program.cs
+++ NumerosInteiros/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NumerosInteiros
+namespace NumerosZeroACem
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            int cont = 1;
+            while (cont <= 100)
+            {
+                Console.WriteLine(cont);
+                cont++;
+
+            }
+            Console.ReadKey();
+
+
+
         }
     }
 }
